a5d854ba882be470beb3b851a74aae5d2fbb34be
Kremeni <sergiusz.hasiuk@gmail.com>
2023-06-02 19:29:48 -0400

    Initial commit

---
+++ build_docker.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\sergi\Documents\GitHub\dockerapps\Auto-GPT"
+docker-compose build auto-gpt
+pause
---
+++ startdocker.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\path\to\your\docker\compose\file"
+docker-compose run autogpt --install-plugin-deps
+pause
